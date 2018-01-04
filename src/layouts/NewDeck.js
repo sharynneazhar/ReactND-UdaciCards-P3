b4507d56f9f7e0da50996bf4d32f58d485d5e4a3
Sharynne Azhar <sharynneazhar@gmail.com>
2018-01-04 14:02:44 -0600

    Update NewDeck view with text input effects

--- src/layouts/NewDeck.js
+++ src/layouts/NewDeck.js
@@ -1,29 +1,76 @@
 import React, { Component } from 'react'
-import { Ionicons } from '@expo/vector-icons'
-import Banner from '../components/Banner'
+import { Sae } from 'react-native-textinput-effects';
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import gs from '../styles'
 import {
-  Animated,
-  Button,
-  ScrollView,
-  StatusBar,
+  Keyboard,
   StyleSheet,
+  Text,
   View,
 } from 'react-native'
 
 export default class NewDeck extends Component {
+  state = {
+    name: '',
+  }
+
+  handleInputChange = (name) => {
+    this.setState({ name })
+  }
+
+  handleSubmit = () => {
+    this.props.navigation.navigate('Deck', {
+      name: this.state.name
+    })
+  }
+
   render() {
+    const { navigation } = this.props
     return (
-      <ScrollView>
-        <Button
-          onPress={() => {}}
-          title="Add Card"
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          What's the title of your deck?
+        </Text>
+        <Sae
+          autoCorrect={false}
+          blurOnSubmit={true}
+          iconClass={MaterialCommunityIcons}
+          iconName={'lead-pencil'}
+          iconColor={gs.colors.blue}
+          inputStyle={styles.input}
+          label='Deck Title'
+          labelStyle={styles.label}
+          onBlur={() => Keyboard.dismiss}
+          onChangeText={this.handleInputChange}
+          onSubmitEditing={this.handleSubmit}
+          returnKeyType='done'
         />
-        <StatusBar barStyle="default" />
-      </ScrollView>
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    paddingTop: 80,
+    paddingLeft: 40,
+    paddingRight: 40,
+    backgroundColor: gs.colors.white,
+  },
+  text: {
+    marginBottom: 50,
+    color: gs.colors.darkGrey,
+    fontFamily: gs.fonts.bold,
+    fontSize: 32,
+  },
+  label: {
+    color: gs.colors.darkGrey,
+    fontFamily: gs.fonts.bold,
+  },
+  input: {
+    color: gs.colors.darkGrey,
+    fontFamily: gs.fonts.bold,
+    fontSize: 20,
+  },
 })
---
+++ src/utils/helpers.js
@@ -0,0 +1,8 @@
+import { NavigationActions } from 'react-navigation'
+
+export const goHome = NavigationActions.reset({
+  index: 0,
+  actions: [
+    NavigationActions.navigate({ routeName: 'Home' })
+  ]
+})
